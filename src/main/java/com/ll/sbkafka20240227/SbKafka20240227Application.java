07169365fd017e90a6e386101494138c2c08b040
jhs512 <jangka512@gmail.com>
2024-02-27 11:19:56 +0900

    work

---
+++ src/main/java/com/ll/sbkafka20240227/SbKafka20240227Application.java
@@ -0,0 +1,13 @@
+package com.ll.sbkafka20240227;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbKafka20240227Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbKafka20240227Application.class, args);
+    }
+
+}
