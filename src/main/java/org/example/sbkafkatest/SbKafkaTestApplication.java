e42f87d1fb8d176ae11e5199c5f24181491969fb
seosang-won <zmdk1205@naver.com>
2024-11-04 21:23:05 +0900

    init

---
+++ src/main/java/org/example/sbkafkatest/SbKafkaTestApplication.java
@@ -0,0 +1,13 @@
+package org.example.sbkafkatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbKafkaTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbKafkaTestApplication.class, args);
+    }
+
+}
